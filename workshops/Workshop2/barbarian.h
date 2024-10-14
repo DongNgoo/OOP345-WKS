338e8a389f0bdcd23faa1fd66c81281f2c9dbdbd
DongN2 <dngo13@myseneca.ca>
2024-10-13 22:38:49 -0400

    refactor: remove barbarian cpp and implement barbarian module into one header[Dngo13]

--- workshops/Workshop2/barbarian.h
+++ workshops/Workshop2/barbarian.h
@@ -2,49 +2,86 @@
 #define SENECA_BARBARIAN_H
 
 #include <iostream>
-#include <string>
-#include "CharacterTpl.h"
-
+#include "characterTpl.h"  
 using namespace std;
 
 namespace seneca {
 
     template<typename T, typename Ability_t, typename Weapon_t>
-    class Barbarian : public CharacterTpl<T> {
-        double m_baseAttack{};
-        double m_baseDefense{};
-
-        Ability_t m_ability{};
-
-		Weapon_t m_weapon[2];
+    class Barbarian : public CharacterTpL<T> 
+    {
+    private:
+        int m_baseDefense{};     
+        int m_baseAttack{};      
+        Ability_t m_ability{};   
+        Weapon_t m_weapon[2]{};  
 
     public:
-        // Constructor
-        Barbarian(const char* name, int healthMax, int baseAttack, int baseDefense,
-            Weapon_t primaryWeapon, Weapon_t secondaryWeapon) : CharacterTpl<T>(name, healthMax)
+        
+        Barbarian(const char* name, int healthMax, int baseAttack, int baseDefense, 
+            Weapon_t primaryWeapon, Weapon_t secondaryWeapon)
+            : CharacterTpL<T>(name, healthMax)
         {
-			m_baseAttack = baseAttack;
-			m_baseDefense = baseDefense;
-            m_weapon[0] = primaryWeapon;
-            m_weapon[1] = secondaryWeapon;
+            m_baseAttack(baseAttack),
+                m_baseDefense(baseDefense),
+                
+                m_weapon[0] = primaryWeapon;
+                m_weapon[1] = secondaryWeapon;
+            
+        }
+
+        
+        int getAttackAmnt() const override{
+            double weapon1Damage = static_cast<double>(m_weapon[0]) / 2;
+            double weapon2Damage = static_cast<double>(m_weapon[1]) / 2;
+            return m_baseAttack + static_cast<int>(weapon1Damage + weapon2Damage);
+        }
+
+        
+        int getDefenseAmnt() const {
+            return m_baseDefense;
+        }
+
+        
+        Character* clone() const override {
+            return new Barbarian<T, Ability_t, Weapon_t>(*this);
         }
 
        
-        int getAttackAmnt() const override;
-      
-        int getDefenseAmnt() const;
+        void attack(Character* enemy) override {
+            cout << this->getName() << " is attacking " << enemy->getName() << ".\n";
+
+          
+            m_ability.useAbility(this);
+
+            
+            int damage = getAttackAmnt();
+
+           
+            m_ability.transformDamageDealt(damage);
+
+            cout << "Barbarian deals " << damage << " melee damage!\n";
 
         
-        Character* clone() const override;
+            enemy->takeDamage(damage);
+        }
 
-        
-        void attack(Character* enemy) override;
+       
+            cout << this->getName() << " is attacked for " << dmg << " damage.\n";
+            cout << "Barbarian has a defense of " << getDefenseAmnt() << ". Reducing damage received.\n";
 
+           
+            dmg -= getDefenseAmnt();
+            dmg = (dmg < 0) ? 0 : dmg;
+
+            
+            m_ability.transformDamageReceived(dmg);
+
+            
+            CharacterTpL<T>::takeDamage(dmg);
         
-        void takeDamage(int dmg) override;
     };
 
-}
+} // namespace seneca
 
-#include "barbarian.h"
 #endif // SENECA_BARBARIAN_H

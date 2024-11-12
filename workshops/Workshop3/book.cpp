68dcaca456502fae441700158fbac69b40e3b6ce
DongN2 <dngo13@myseneca.ca>
2024-11-12 02:26:04 -0500

    feat: implement book module [Dngo13]

--- workshops/Workshop3/book.cpp
+++ workshops/Workshop3/book.cpp
@@ -1,12 +1,53 @@
 #include "book.h"
+#include "mediaItem.h"
+#include <sstream>
+#include <stdexcept>
+#include <algorithm>
 namespace seneca {
 	//display function
-	Book::Book* createItem(const std::string& strBook) {
+    void Book::display(std::ostream& out) const
+    {
+        if (g_settings.m_tableView)
+        {
+            out << "B | ";
+            out << std::left << std::setfill('.');
+            out << std::setw(50) << this->getTitle() << " | ";
+            out << std::right << std::setfill(' ');
+            out << std::setw(2) << this->m_country << " | ";
+            out << std::setw(4) << this->getYear() << " | ";
+            out << std::left;
+            if (g_settings.m_maxSummaryWidth > -1)
+            {
+                if (static_cast<short>(this->getSummary().size()) <= g_settings.m_maxSummaryWidth)
+                    out << this->getSummary();
+                else
+                    out << this->getSummary().substr(0, g_settings.m_maxSummaryWidth - 3) << "...";
+            }
+            else
+                out << this->getSummary();
+            out << std::endl;
+        }
+        else
+        {
+            size_t pos = 0;
+            out << this->getTitle() << " [" << this->getYear() << "] [";
+            out << m_author << "] [" << m_country << "] [" << m_price << "]\n";
+            out << std::setw(this->getTitle().size() + 7) << std::setfill('-') << "" << '\n';
+            while (pos < this->getSummary().size())
+            {
+                out << "    " << this->getSummary().substr(pos, g_settings.m_maxSummaryWidth) << '\n';
+                pos += g_settings.m_maxSummaryWidth;
+            }
+            out << std::setw(this->getTitle().size() + 7) << std::setfill('-') << ""
+                << std::setfill(' ') << '\n';
+        }
+    }
+    Book* Book::createItem(const std::string& strBook) {
 		if (strBook.size() == 0 || strBook[0] == '#') {
 			throw "Not a valid book";
 		}
 		std::string tittle, author, country, summary;
-		usigned short year;
+		unsigned short year;
 		double price;
 		size_t pos;
 		size_t nextPos = 0;
@@ -16,18 +57,35 @@ namespace seneca {
 		MediaItem::trim(tittle);
 		pos = nextPos + 1;
 
+        //author
 		nextPos = strBook.find(",", pos);
 		author = strBook.substr(pos, nextPos - pos);
 		MediaItem::trim(author);
 		pos = nextPos + 1;
 
+        //country
 		nextPos = strBook.find(",", pos);
 		country = strBook.substr(pos, nextPos - pos);
 		MediaItem::trim(country);
 		pos = nextPos + 1;
+        // Price
+        nextPos = strBook.find(",", pos);
+        std::string priceStr = strBook.substr(pos, nextPos - pos);
+        MediaItem::trim(priceStr);
+        price = std::stod(priceStr);
+        pos = nextPos + 1;
 
-		summary = strBook.substr(pos, nextPos = pos)
+        // Year
+        nextPos = strBook.find(",", pos);
+        std::string yearStr = strBook.substr(pos, nextPos - pos);
+        MediaItem::trim(yearStr);
+        year = static_cast<unsigned short>(std::stoi(yearStr));
+        pos = nextPos + 1;
 
-		return nullptr;
+        summary = strBook.substr(pos, nextPos = pos);
+        MediaItem::trim(summary);
+
+		//return nullptr;
+        return new Book(author, tittle, country, price, year, summary);
 	}
 }
--- workshops/Workshop3/book.h
+++ workshops/Workshop3/book.h
@@ -1,13 +1,26 @@
 #ifndef SENECA_BOOK_H
 #define SENECA_BOOK_H
 #include "mediaItem.h"
-namespace seneca {
-	class Book :: public MediaItem {
-		std::string m_author{ };
-		std::sting m_country{};
+#include <iostream>
+#include <iomanip>
+#include "settings.h"
+namespace seneca 
+{
+
+	class Book : public MediaItem {
+	private:
+		std::string m_author;
+		std::string m_country;
+		double m_price;
+		// Private constructor (only accessible within the class)
+		Book::Book(const std::string& author, const std::string& title, const std::string& country,
+			double price, unsigned short year, const std::string& summary)
+			: MediaItem(title, summary, year), // Pass title, summary, and year to MediaItem
+			m_author(author), m_country(country), m_price(price) { }
+	public:
+		Book* createItem(const std::string& strBook);
 	};
 
-public:
-	Book* createItem(const std::string& strBook);
+
 }
 #endif

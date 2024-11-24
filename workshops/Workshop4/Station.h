c672abecbc90e3061ef5c057dacc787d6b19af22
DongN2 <dngo13@myseneca.ca>
2024-11-24 00:19:43 -0500

    feat:Implement Station header[Dngo13]

--- workshops/Workshop4/Station.h
+++ workshops/Workshop4/Station.h
@@ -1,4 +1,35 @@
 #ifndef SENECA_STATION_H
 #define SENECA_STATION_H
+#include <string>
+#include <iostream>
+#include "Utilities.h"
+namespace seneca {
+
+	class Station {
+		int m_id;
+		std::string m_itemName;
+		std::string m_description;
+		size_t m_serialNumber;
+		size_t m_quantity;
+
+		static size_t m_widthField;
+		static int id_generator;
+
+	public:
+		//Customer 1-arguement construstor
+
+		Station(const std::string& record);
+
+		const std::string& getItemName() const;
+
+		size_t getNextSerialNumber();
+
+		size_t getQuantity() const;
+
+		void updateQuantity();
+
+		void display(std::ostream& os, bool full) const;
+	};
+}
 
 #endif
